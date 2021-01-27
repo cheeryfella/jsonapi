5bfb2ff726c30d6ca7c15a41ba83ccd8eac0cfe5
cheeryfella <cheeryfella@gmail.com>
2021-01-27 00:08:55 +0000

    Initial commit starting from github.com/google/jsonapi @22/10/2020

---
+++ errors_test.go
@@ -0,0 +1,118 @@
+package jsonapi_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+
+	"github.com/cheeryfella/jsonapi"
+)
+
+func TestErrorObjectWritesExpectedErrorMessage(t *testing.T) {
+	input := &jsonapi.ErrorObject{Title: "Title test.", Detail: "Detail test."}
+
+	output := input.Error()
+
+	if output != fmt.Sprintf("Error: %s %s\n", input.Title, input.Detail) {
+		t.Fatal("Unexpected output.")
+	}
+}
+
+func TestMarshalErrorsWritesTheExpectedPayload(t *testing.T) {
+	var marshalErrorsTableTasts = map[string]struct {
+		In    []*jsonapi.ErrorObject
+		Out   map[string]interface{}
+	}{
+		"TestFieldsAreSerializedAsNeeded": {
+			In:    []*jsonapi.ErrorObject{{
+				ID: "0",
+				Title: "Test title.",
+				Detail: "Test detail",
+				Status: "400",
+				Code: "E1100",
+			}},
+			Out: map[string]interface{}{
+				"errors": []interface{}{map[string]interface{}{
+					"id": "0",
+					"title": "Test title.",
+					"detail": "Test detail",
+					"status": "400",
+					"code": "E1100",
+				}},
+			},
+		},
+		"TestMetaFieldIsSerializedProperly": {
+			In:    []*jsonapi.ErrorObject{{
+				Title: "Test title.",
+				Detail: "Test detail",
+				Meta: &map[string]interface{}{
+					"key": "val",
+				},
+			}},
+			Out: map[string]interface{}{
+				"errors": []interface{}{map[string]interface{}{
+					"title": "Test title.",
+					"detail": "Test detail",
+					"meta": map[string]interface{}{
+						"key": "val",
+					}},
+				},
+			},
+		},
+		"TestSourceFieldIsSerializedProperly": {
+			In:    []*jsonapi.ErrorObject{{
+				Title: "Test title.",
+				Detail: "Test detail",
+				Source: &jsonapi.ErrorSource{
+					Pointer:   "/data/attributes/field",
+					Parameter: "filter",
+				},
+			}},
+			Out: map[string]interface{}{
+				"errors": []interface{}{map[string]interface{}{
+					"title": "Test title.",
+					"detail": "Test detail",
+					"source": map[string]interface{}{
+						"parameter": "filter",
+						"pointer": "/data/attributes/field",
+					},
+				},
+			}},
+		},
+		"TestLinksFieldIsSerializedProperly": {
+			In:    []*jsonapi.ErrorObject{{
+				Title: "Test title.",
+				Detail: "Test detail",
+				Links: &jsonapi.ErrorLink{
+					About: "/url/to/details",
+				},
+			}},
+			Out: map[string]interface{}{
+				"errors": []interface{}{map[string]interface{}{
+					"title": "Test title.",
+					"detail": "Test detail",
+					"links": map[string]interface{}{
+						"about": "/url/to/details",
+					},
+				}},
+			},
+		},
+	}
+
+	for name, test := range marshalErrorsTableTasts {
+		t.Run(name, func(t *testing.T) {
+			buffer, output := bytes.NewBuffer(nil), map[string]interface{}{}
+			var writer io.Writer = buffer
+
+			_ = jsonapi.MarshalErrors(writer, test.In)
+			json.Unmarshal(buffer.Bytes(), &output)
+
+			if !reflect.DeepEqual(output, test.Out) {
+				t.Fatalf("Expected: \n%#v \nto equal: \n%#v", output, test.Out)
+			}
+		})
+	}
+}
---
+++ models_test.go
@@ -0,0 +1,195 @@
+package jsonapi_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cheeryfella/jsonapi"
+)
+
+type BadModel struct {
+	ID int `jsonapi:"primary"`
+}
+
+type ModelBadTypes struct {
+	ID           string     `jsonapi:"primary,badtypes"`
+	StringField  string     `jsonapi:"attr,string_field"`
+	FloatField   float64    `jsonapi:"attr,float_field"`
+	TimeField    time.Time  `jsonapi:"attr,time_field"`
+	TimePtrField *time.Time `jsonapi:"attr,time_ptr_field"`
+}
+
+type WithPointer struct {
+	ID       *uint64  `jsonapi:"primary,with-pointers"`
+	Name     *string  `jsonapi:"attr,name"`
+	IsActive *bool    `jsonapi:"attr,is-active"`
+	IntVal   *int     `jsonapi:"attr,int-val"`
+	FloatVal *float32 `jsonapi:"attr,float-val"`
+}
+
+type Timestamp struct {
+	ID   int        `jsonapi:"primary,timestamps"`
+	Time time.Time  `jsonapi:"attr,timestamp,iso8601"`
+	Next *time.Time `jsonapi:"attr,next,iso8601"`
+}
+
+type Car struct {
+	ID    *string `jsonapi:"primary,cars"`
+	Make  *string `jsonapi:"attr,make,omitempty"`
+	Model *string `jsonapi:"attr,model,omitempty"`
+	Year  *uint   `jsonapi:"attr,year,omitempty"`
+}
+
+type Post struct {
+	Blog
+	ID            uint64     `jsonapi:"primary,posts"`
+	BlogID        int        `jsonapi:"attr,blog_id"`
+	ClientID      string     `jsonapi:"client-id"`
+	Title         string     `jsonapi:"attr,title"`
+	Body          string     `jsonapi:"attr,body"`
+	Comments      []*Comment `jsonapi:"relation,comments"`
+	LatestComment *Comment   `jsonapi:"relation,latest_comment"`
+}
+
+type Comment struct {
+	ID       int    `jsonapi:"primary,comments"`
+	ClientID string `jsonapi:"client-id"`
+	PostID   int    `jsonapi:"attr,post_id"`
+	Body     string `jsonapi:"attr,body"`
+}
+
+type Book struct {
+	ID          uint64  `jsonapi:"primary,books"`
+	Author      string  `jsonapi:"attr,author"`
+	ISBN        string  `jsonapi:"attr,isbn"`
+	Title       string  `jsonapi:"attr,title,omitempty"`
+	Description *string `jsonapi:"attr,description"`
+	Pages       *uint   `jsonapi:"attr,pages,omitempty"`
+	PublishedAt time.Time
+	Tags        []string `jsonapi:"attr,tags"`
+}
+
+type Blog struct {
+	ID            int       `jsonapi:"primary,blogs"`
+	ClientID      string    `jsonapi:"client-id"`
+	Title         string    `jsonapi:"attr,title"`
+	Posts         []*Post   `jsonapi:"relation,posts"`
+	CurrentPost   *Post     `jsonapi:"relation,current_post"`
+	CurrentPostID int       `jsonapi:"attr,current_post_id"`
+	CreatedAt     time.Time `jsonapi:"attr,created_at"`
+	ViewCount     int       `jsonapi:"attr,view_count"`
+}
+
+func (b *Blog) JSONAPILinks() *jsonapi.Links {
+	return &jsonapi.Links{
+		"self": fmt.Sprintf("https://example.com/api/blogs/%d", b.ID),
+		"comments": jsonapi.Link{
+			Href: fmt.Sprintf("https://example.com/api/blogs/%d/comments", b.ID),
+			Meta: jsonapi.Meta{
+				"counts": map[string]uint{
+					"likes":    4,
+					"comments": 20,
+				},
+			},
+		},
+	}
+}
+
+func (b *Blog) JSONAPIRelationshipLinks(relation string) *jsonapi.Links {
+	if relation == "posts" {
+		return &jsonapi.Links{
+			"related": jsonapi.Link{
+				Href: fmt.Sprintf("https://example.com/api/blogs/%d/posts", b.ID),
+				Meta: jsonapi.Meta{
+					"count": len(b.Posts),
+				},
+			},
+		}
+	}
+	if relation == "current_post" {
+		return &jsonapi.Links{
+			"self": fmt.Sprintf("https://example.com/api/posts/%s", "3"),
+			"related": jsonapi.Link{
+				Href: fmt.Sprintf("https://example.com/api/blogs/%d/current_post", b.ID),
+			},
+		}
+	}
+	return nil
+}
+
+func (b *Blog) JSONAPIMeta() *jsonapi.Meta {
+	return &jsonapi.Meta{
+		"detail": "extra details regarding the blog",
+	}
+}
+
+func (b *Blog) JSONAPIRelationshipMeta(relation string) *jsonapi.Meta {
+	if relation == "posts" {
+		return &jsonapi.Meta{
+			"this": map[string]interface{}{
+				"can": map[string]interface{}{
+					"go": []interface{}{
+						"as",
+						"deep",
+						map[string]interface{}{
+							"as": "required",
+						},
+					},
+				},
+			},
+		}
+	}
+	if relation == "current_post" {
+		return &jsonapi.Meta{
+			"detail": "extra current_post detail",
+		}
+	}
+	return nil
+}
+
+type BadComment struct {
+	ID   uint64 `jsonapi:"primary,bad-comment"`
+	Body string `jsonapi:"attr,body"`
+}
+
+func (bc *BadComment) JSONAPILinks() *jsonapi.Links {
+	return &jsonapi.Links{
+		"self": []string{"invalid", "should error"},
+	}
+}
+
+type Company struct {
+	ID        string    `jsonapi:"primary,companies"`
+	Name      string    `jsonapi:"attr,name"`
+	Boss      Employee  `jsonapi:"attr,boss"`
+	Teams     []Team    `jsonapi:"attr,teams"`
+	FoundedAt time.Time `jsonapi:"attr,founded-at,iso8601"`
+}
+
+type Team struct {
+	Name    string     `jsonapi:"attr,name"`
+	Leader  *Employee  `jsonapi:"attr,leader"`
+	Members []Employee `jsonapi:"attr,members"`
+}
+
+type Employee struct {
+	Firstname string     `jsonapi:"attr,firstname"`
+	Surname   string     `jsonapi:"attr,surname"`
+	Age       int        `jsonapi:"attr,age"`
+	HiredAt   *time.Time `jsonapi:"attr,hired-at,iso8601"`
+}
+
+type CustomIntType int
+type CustomFloatType float64
+type CustomStringType string
+
+type CustomAttributeTypes struct {
+	ID string `jsonapi:"primary,customtypes"`
+
+	Int        CustomIntType  `jsonapi:"attr,int"`
+	IntPtr     *CustomIntType `jsonapi:"attr,intptr"`
+	IntPtrNull *CustomIntType `jsonapi:"attr,intptrnull"`
+
+	Float  CustomFloatType  `jsonapi:"attr,float"`
+	String CustomStringType `jsonapi:"attr,string"`
+}
